462d9ce27092fe4965d4d4a7b0a6c672be0f84bc
tantawyamirzada138 <tantawyamirzada138@gmail.com>
2025-03-16 23:05:42 +0000

    Add 11.js

---
+++ src/11.js
@@ -0,0 +1,17 @@
+const mathHomework = {
+  solution1: function() {
+    let x = 5;
+    const y = 3;
+    return x + y;
+  },
+  solution2: function() {
+    let x = 10;
+    const y = 4;
+    return x * y;
+  },
+  solution3: function() {
+    let x = 15;
+    const y = 5;
+    return x / y;
+  }
+};
