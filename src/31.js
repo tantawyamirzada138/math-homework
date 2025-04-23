944747354d06081fa4f2db0b561bca154e22354f
tantawyamirzada138 <tantawyamirzada138@gmail.com>
2025-04-23 10:20:46 +0000

    Add 31.js

---
+++ src/31.js
@@ -0,0 +1,20 @@
+// Problem 1: Basic Math Operations
+
+function addNumbers(a, b) {
+    return a + b;
+}
+
+function subtractNumbers(a, b) {
+    return a - b;
+}
+
+function multiplyNumbers(a, b) {
+    return a * b;
+}
+
+function divideNumbers(a, b) {
+    if (b === 0) {
+        throw new Error("Cannot divide by zero.");
+    }
+    return a / b;
+}
