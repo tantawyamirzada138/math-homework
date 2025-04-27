573afacda950a3158fb35dd7b171cb9922c349fa
tantawyamirzada138 <tantawyamirzada138@gmail.com>
2025-04-27 01:49:32 +0000

    Add 34.js

---
+++ src/34.js
@@ -0,0 +1,15 @@
+function calculateSum(a, b) {
+  return a + b;
+}
+
+function findMax(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
+
+function printMessage(message) {
+  console.log(message);
+}
